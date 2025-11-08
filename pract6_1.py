123fa6a6672dea6f4dc56903fa1467527fa5c70e
W1sent1 <ismonovhokim4@gmail.com>
2025-11-08 23:48:07 +0300

    Update pract6_1.py

--- pract6_1.py
+++ pract6_1.py
@@ -4,6 +4,7 @@ duplicates = {}
 for i, num in enumerate(arr):
     duplicates[num] = duplicates.get(num, []) + [i]
 
+print("\n Одинаковые элементы:")
 for num, indices in duplicates.items():
     if len(indices) > 1:
         print(f"Число {num}: индексы {indices}")
